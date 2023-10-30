5ac375fcbba4e600c74611a90e75ee63227312fc
eheimark <eheimark@users.noreply.github.com>
2023-10-30 00:03:51 +0000

    Add files via upload

---
+++ HelloWorld.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo -e "\nPositive news...";
+sleep 2;
+echo -e "\n
+\n           (((       ,,,
+\n           ( *)=====/   \====
+\n __________/  )    / it's\ 
+\n \__          /   /    a  \ 
+\n   \____    _/   /  girl...\ 
+\n      / \__/     (ELIZABETH )
+\n     /____|__/_/  \ HEIMARK/
+\n          |        \______/ 
+\n          |        
+\n         _|_
+\n          \ \  ";
+sleep 3;
+echo -e "\nBorn Saturday 28th October
+\n  weighing 8lbs 1oz";
+sleep 3;
+echo -e "
+\n             _,_  llllllllllll\ 
+\n___         /   \  llllllllllll\ 
+\n   \       | @ @ |  llllllllllll\ 
+\n    \-----oOO---Ooo-------------/
+\n     \lllllllllllllllllllllllll/
+\n      \lllllllllllllllllllllll/
+\n       \lllllllllllllllllllll/
+\n            /             \  
+\n           ||             ||
+\n           OO             OO
+\n";
+sleep 5;
+open https://imgur.com/a/vR7td3O;
